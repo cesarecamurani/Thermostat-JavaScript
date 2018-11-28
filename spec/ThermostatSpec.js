bf95f9c506834b6b3f3c2390664f3f9a6ddea8f7
cesare camurani <cesare.camurani@gmail.com>
2018-11-28 16:10:13 +0000

    minimum temperature added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,6 +11,10 @@ describe('Thermostat', function() {
     expect(thermostat.temperature()).toEqual(20);
   });
 
+  it('sets the minimum temperature to 10 degrees', function() {
+    expect(thermostat.minTemperature()).toEqual(10);
+  });
+
   it('increase the temperature by 1 degree', function() {
     thermostat.up();
     expect(thermostat.temperature()).toEqual(21);
@@ -26,6 +30,13 @@ describe('Thermostat', function() {
     thermostat.reset();
     expect(thermostat.temperature()).toEqual(20);
   });
+
+  it('blocks the temperature at a minimum of 10 degrees', function() {
+    for(var i=0; i < 11; i++) { thermostat.down(); }
+    expect(thermostat.temperature()).toEqual(10)
+  });
+
+
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,18 +2,28 @@
 
 function Thermostat() {
   this._temperature = 20;
+  this._minTemp = 10;
 };
 
 Thermostat.prototype.temperature = function() {
   return this._temperature;
 };
 
+Thermostat.prototype.minTemperature = function() {
+  return this._minTemp;
+};
+
 Thermostat.prototype.up = function() {
-  return this._temperature++;
+  return this._temperature++ ;
 };
 
 Thermostat.prototype.down = function() {
-  return this._temperature--;
+  if(this._minTemp < this._temperature) {
+    return this._temperature-- ;
+  }
+  else {
+    return this._minTemp;
+  }
 };
 
 Thermostat.prototype.reset = function() {
