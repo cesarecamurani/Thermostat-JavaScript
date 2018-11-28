80e3481d091bec2935cfc5d72e29b1c6a8a6bcf5
cesare camurani <cesare.camurani@gmail.com>
2018-11-28 17:30:48 +0000

    maximum temperature added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,34 +7,78 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('starts with a default temperature of 20 degrees', function() {
-    expect(thermostat.temperature()).toEqual(20);
+  describe('initial settings', function() {
+
+    it('starts with a default temperature of 20 degrees', function() {
+      expect(thermostat.temperature()).toEqual(20);
+    });
+
+    it('sets the minimum temperature to 10 degrees', function() {
+      expect(thermostat.minTemp()).toEqual(10);
+    });
+
+    it('sets the maximum temperature to 25 degrees', function() {
+      expect(thermostat.maxTemp()).toEqual(25);
+    });
+
+    it('checks the initial power mode setting', function() {
+      expect(thermostat.initialPowerMode()).toEqual('On');
+    });
+
   });
 
-  it('sets the minimum temperature to 10 degrees', function() {
-    expect(thermostat.minTemperature()).toEqual(10);
+  describe('temperature up', function() {
+
+    it('increases the temperature by 1 degree', function() {
+      thermostat.up();
+      expect(thermostat.temperature()).toEqual(21);
+    });
+
   });
 
-  it('increase the temperature by 1 degree', function() {
-    thermostat.up();
-    expect(thermostat.temperature()).toEqual(21);
+  describe('temperature down', function() {
+
+    it('lowers the temperature by 1 degree', function() {
+      thermostat.down();
+      expect(thermostat.temperature()).toEqual(19);
+    });
+
+    it('blocks the temperature at a minimum of 10 degrees', function() {
+      for(var i=0; i < 10; i++) { thermostat.down(); }
+      thermostat.down();
+      expect(thermostat.temperature()).toEqual(10)
+    });
+
   });
 
-  it('increase the temperature by 1 degree', function() {
-    thermostat.down();
-    expect(thermostat.temperature()).toEqual(19);
+  describe('reset temperature', function() {
+
+    it('reset the temperature to 20 degrees', function() {
+      thermostat.up();
+      thermostat.reset();
+      expect(thermostat.temperature()).toEqual(20);
+    });
+
   });
 
-  it('increase the temperature by 1 degree', function() {
-    thermostat.up();
-    thermostat.reset();
-    expect(thermostat.temperature()).toEqual(20);
+  describe('change the power mode', function() {
+
+    it('sets the power saving mode to on', function() {
+      thermostat.powerSavingModeOn();
+      expect(thermostat.powerSavingModeOn()).toEqual('On');
+    });
+
+    it('sets the power saving mode to off', function() {
+      thermostat.powerSavingModeOff();
+      expect(thermostat.powerSavingModeOff()).toEqual('Off');
+    });
+
   });
 
-  it('blocks the temperature at a minimum of 10 degrees', function() {
-    for(var i=0; i < 11; i++) { thermostat.down(); }
-    expect(thermostat.temperature()).toEqual(10)
-  });
+
+
+
+
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,14 +3,34 @@
 function Thermostat() {
   this._temperature = 20;
   this._minTemp = 10;
+  this._maxTemp = 25;
+  this._powerSaving = 'On';
 };
 
 Thermostat.prototype.temperature = function() {
   return this._temperature;
 };
 
-Thermostat.prototype.minTemperature = function() {
+Thermostat.prototype.minTemp = function() {
   return this._minTemp;
+};
+
+Thermostat.prototype.maxTemp = function() {
+  return this._maxTemp;
+};
+
+Thermostat.prototype.initialPowerMode = function() {
+  return this._powerSaving;
+};
+
+Thermostat.prototype.powerSavingModeOn = function() {
+  return this._powerSaving = 'On';
+  return this._maxTemp = 25;
+};
+
+Thermostat.prototype.powerSavingModeOff = function() {
+  return this._powerSaving = 'Off';
+  return this._maxTemp = 32;
 };
 
 Thermostat.prototype.up = function() {
